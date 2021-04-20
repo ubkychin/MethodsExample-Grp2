cfa2ca814194a02c89a160dab717993c8acfe8ce
Anh Nguyen <anguyen@swin.edu.au>
2021-04-20 15:02:36 +1000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MethodsExample_Grp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            LoginMenu();
+        }
+
+        public static string HelloName(string name) {
+            string sentence = "Hello " + name;
+            return sentence;
+        }
+
+        public static void LoginMenu() {
+            System.Console.Write("username: ");
+            string user = Console.ReadLine();
+
+            System.Console.Write("password: ");
+            string pass = Console.ReadLine();
+
+            bool loginResult = Login(user, pass);
+            if(loginResult) {
+                System.Console.WriteLine("Successful Login");
+            } else {
+                System.Console.WriteLine("Unsuccessful Login");
+            }
+
+        }
+
+        public static bool Login(string username, string password) {
+            string validUsername = "Clive";
+            string validPassword = "abc123";
+
+            if (username == validUsername && password == validPassword) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
